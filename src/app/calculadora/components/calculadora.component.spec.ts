edc5da09f8ca69fd0c7d7c02e9f4fd1d37f6cd72
Henrique Maike <50559406+HenriqueMaike@users.noreply.github.com>
2022-06-25 22:11:26 -0300

    first commit

---
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CalculadoraComponent } from './calculadora.component';
+import { CalculadoraService } from '../services';
+
+describe('CalculadoraComponent', () => {
+  let component: CalculadoraComponent;
+  let fixture: ComponentFixture<CalculadoraComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CalculadoraComponent ],
+      providers: [
+        CalculadoraService
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CalculadoraComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/calculadora/services/index.ts
@@ -0,0 +1,2 @@
+export * from './calculadora.service';
+
